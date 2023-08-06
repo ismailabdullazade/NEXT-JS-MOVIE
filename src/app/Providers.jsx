39293b907239a16692a85cea99e351219b4907e0
IsmailGN <84373556+IsmailGN@users.noreply.github.com>
2023-08-06 20:16:32 +0400

    first commit structure

---
+++ src/app/Providers.jsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+import { ThemeProvider } from 'next-themes'
+
+const Providers = ({children}) => {
+  return (
+    <ThemeProvider>
+        {children}
+    </ThemeProvider>
+  )
+}
+
+export default Providers
